d496053b9d0fb72756412d53fe0bc417e2fd3ebd
kherineldo <hduntitled@gmail.com>
2024-06-24 16:24:50 +0400

    initial commit

---
+++ app/schemas.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+from typing import Annotated
+
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        from_attributes = True
+
+
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserLogin(BaseModel):
+    email : EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: Annotated[int, Field(le=1)]
+
+
